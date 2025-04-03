cea06db77a4d455f0d094a85f0b696876865614b
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2025-04-03 18:49:50 -0500

    add_reactRouter

---
+++ my-react-app/src/pages/Orders.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+const Orders = () => {
+    return(
+        <div>
+            <Link to="/products">PRODUCTSSSS</Link>
+        </div>
+    )
+}
---
+++ my-react-app/src/pages/Start.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+const Start = () => {
+    return(
+        <div>
+            <Link to="/products">PRODUCTSSSS</Link>
+        </div>
+    )
+}
